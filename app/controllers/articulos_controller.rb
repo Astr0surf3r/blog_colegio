bfcfc179f372c393230d186aa7613b39ad7f3532
Astr0surf3r <astr0surf3r@gmail.com>
2015-04-17 19:24:58 +0200

    ahora podemos guardar el objecto Articulo

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -1,12 +1,32 @@
 class ArticulosController < ApplicationController
   
+  #before_action :set_params, :only => [:create]
+
   def new
   	@articulo = Articulo.new
   end
 
   def create
+  	@articulo = Articulo.new(articulo_params)
+
+  	if @articulo.save
+
+  		redirect_to @articulo
+
+  	else
+
+  		render 'new'
+
+  	end
+
   end
 
   def show
+  end
+
+  private
+
+  def articulo_params
+  	params.require(:articulo).permit(:titulo, :descripcion)
   end
 end
