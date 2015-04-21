57ba420dfd73221af2ea65bddc9e6e935b571fa0
Astr0surf3r <astr0surf3r@gmail.com>
2015-04-21 19:51:35 +0200

    creo y borro sesion egracias a los enlace de la navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :usuario_autenticado, :autenticado?
+  
+  def usuario_autenticado
+  	@current_user ||= Usuario.find(session[:user]) if session[:user]
+  end
+
+  def autenticado?
+
+  	!!usuario_autenticado #true
+
+  end
+
 end
